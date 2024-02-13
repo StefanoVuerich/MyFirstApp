a347cb6b24ff41f7991196f97a2e55f106f0301d
Stefano Vuerich <vuerich@irsweb.it>
2024-02-13 16:26:13 +0100

    Add project files.

---
+++ MyFirstApp/Persona.cs
@@ -0,0 +1,16 @@
+﻿namespace MyFirstApp
+{
+    using System;
+
+    public class Persona
+    {
+        public string Nome { get; set; }
+        public string Cognome { get; set; }
+        public int Eta { get; set; }
+
+        internal void StampaInformazioni()
+        {
+            Console.WriteLine($"{this.Nome}, {this.Cognome}, {this.Eta}");
+        }
+    }
+}
---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MyFirstApp;
+
+Persona persona = new Persona();
+
+Console.WriteLine("Inserisci nome");
+persona.Nome = Console.ReadLine();
+
+Console.WriteLine("Inserisci cognome");
+persona.Cognome = Console.ReadLine();
+
+Console.WriteLine("Inserisci età");
+persona.Eta = Convert.ToInt32(Console.ReadLine());
+
+persona.StampaInformazioni();
+
+Console.ReadKey();
